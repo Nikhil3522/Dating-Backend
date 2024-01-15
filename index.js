2004cfd978c6808777830c4a50c915cfff09a0f2
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-15 20:37:20 +0530

    updated index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(session({
     cookie: {
         sameSite: 'None',
         secure: true,
-        domain: 'onrender.com'
+        domain: 'frontend-datuni.onrender.com'
         // maxAge: (1000 * 60 * 100)
     }
 }));
