9bc4e1f333db4bf9faff0312997f602ce5b477d7
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-17 22:30:35 +0530

    CORS update

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const http = require('http');
 
 
 router.use(cors({
-    origin: ['https://www.dateuni.in', 'https://dateuni.in', '*'],
+    origin: ['https://www.dateuni.in', 'https://dateuni.in', 'https://dating-frontend-sigma.vercel.app', '*'],
     methods: ['GET', 'POST'],
     credentials: true // Allow credentials (e.g., cookies, authorization headers)
   }));
