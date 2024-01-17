108414d008eafb5e4be30fc85bc65b8b337a8946
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-17 21:20:59 +0530

    host

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use('/assets', express.static('assets'));
 
 app.use('/', require('./routes'));
 
-app.listen(port, function (err) {
+app.listen(port, "0.0.0.0" , function (err) {
     if (err) {
         console.log('Error in running the server', err);
     }
