8d36c24e2f78fecb4ac6f5e9b42a52ed1446271b
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-17 21:16:56 +0530

    produnction port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const http = require('http');
 // const socketIO = require('socket.io');
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.urlencoded());
 app.use(express.static('assets'));
